e46118eb14cd953f68bd4ca899066b73e1526676
Gabriel <gabriel.svasc99@gmail.com>
2023-02-04 23:12:23 -0300

    feat(mediator): implementação do método comportamental

---
+++ app/Comportamental/mediator.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+from abc import ABC
+
+
+class IMediator(ABC):
+    """
+    Interface utilizada na classe mediadora, que implementa um método
+    notify.
+    """
+
+    def notify(self, sender: object, event: str) -> None:
+        pass
+
+
+class ConcreteMediator(IMediator):
+    def __init__(self, component1: Component1, component2: Component2) -> None:
+        self._component1 = component1
+        self._component1.mediator = self
+        self._component2 = component2
+        self._component2.mediator = self
+
+    def notify(self, sender: object, event: str) -> None:
+        if event == "A":
+            self._component2.do_c()
+        elif event == "D":
+            self._component1.do_b()
+            self._component2.do_c()
+
+
+class BaseComponent:
+    """
+    Componente base responsável por associar um mediador.
+    """
+
+    def __init__(self, mediator: IMediator = None) -> None:
+        self._mediator = mediator
+
+    @property
+    def mediator(self) -> IMediator:
+        return self._mediator
+
+    @mediator.setter
+    def mediator(self, mediator: IMediator) -> None:
+        self._mediator = mediator
+
+
+class Component1(BaseComponent):
+    def do_a(self) -> None:
+        print("Component1: Fazendo A...")
+        self.mediator.notify(self, "A")
+
+    def do_b(self) -> None:
+        print("Component1: Fazendo B...")
+        self.mediator.notify(self, "B")
+
+
+class Component2(BaseComponent):
+    def do_c(self) -> None:
+        print("Component2: Fazendo C...")
+        self.mediator.notify(self, "C")
+
+    def do_d(self) -> None:
+        print("Component2: Fazendo D...")
+        self.mediator.notify(self, "D")
+
+
+if __name__ == "__main__":
+    c1 = Component1()
+    c2 = Component2()
+    mediator = ConcreteMediator(c1, c2)
+
+    # do_a > notify > do_c
+    c1.do_a()
+
+    # do_d > notify > do_b > do_b
+    c2.do_d()
